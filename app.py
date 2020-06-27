923bb245fdb14203acd05bc6c5b79c90cbd82c84
kunkun <2749320352@qq.com>
2020-06-27 13:24:37 +0800

    添加了hello页面

--- app.py
+++ app.py
@@ -4,13 +4,17 @@ from flask import render_template as rp
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
     return rp("index.html")
     # 就可以显示index.html里的内容了！
 
 @app.route('/move')
 def move():
     return rp("/test/move.html")
+
+@app.route('/hello')
+def hello():
+    return rp("/test/hello.html")
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port="5000", threaded=True) #多线程
