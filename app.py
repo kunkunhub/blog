5b31a491d16441ab7066e8a413e3e2efbfe33262
kunkun <2749320352@qq.com>
2020-07-15 16:05:53 +0800

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
@@ -10,11 +11,14 @@ class IndexHandler(tornado.web.RequestHandler): # 继承
     def get(self):                              # 处理的请求方法
         msg = self.get_argument('msg', 'Hello, World!')   
         # 获取请求参数"msg"默认为"Hello"
-        self.write(msg)   # 输出
+        self.render("index.html")   # 输出
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()    # 解析命令行
-    app = tornado.web.Application(handlers=[(r"/", IndexHandler)]) 
+    app = tornado.web.Application(
+        handlers = [(r"/", IndexHandler)],
+        template_path=os.path.join(os.path.dirname(__file__), "templates")
+    ) 
     # !handlers=[(r"<url>", <类>)]
     http_server = tornado.httpserver.HTTPServer(app) # !
     http_server.listen(options.port)    # !!
